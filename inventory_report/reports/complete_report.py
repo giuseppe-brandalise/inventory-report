a7dcf347e6fb6286922016104d15b93e6e01a83e
Giuseppe <giuseppebrandalise@gmail.com>
2023-11-22 17:49:16 -0300

    Requisito 9

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,27 @@
-class CompleteReport:
-    pass
+from inventory_report.report.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    def generate(self) -> str:
+        return (
+            f"{super.generate()}"
+            f"Stocked products by company:"
+            f"{self.get_companies_stocked()}"
+        )
+
+    def get_companies_stocked(self) -> str:
+        company_stocked: dict[str, int] = {}
+
+        for inventory in self.inventory:
+            for product in inventory.data:
+                if product.company_name in company_stocked:
+                    company_stocked[product.company_name] += 1
+                else:
+                    company_stocked[product.company_name] = 1
+
+        amount_per_company = ""
+
+        for company, stocked in company_stocked.items():
+            amount_per_company += f"- {company}: {stocked}\n"
+
+        return amount_per_company
