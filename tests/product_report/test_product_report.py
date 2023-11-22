1af1c8cfc9fe4327177bfb3398fe11c92c5cd4db
Giuseppe <giuseppebrandalise@gmail.com>
2023-11-22 15:54:25 -0300

    Requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,17 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product("5", "gas mask", "amazon", "2023", "2027", "1035",
+                      "store in dry spaces")
+
+    assert (
+        str(product)
+        == "The product 5 - gas mask "
+        "with serial number 1035 "
+        "manufactured on 2023 "
+        "by the company amazon "
+        "valid until 2027 "
+        "must be stored according to the following instructions: "
+        "store in dry spaces."
+    )
